d40b94336a34a24933c61cc6e180a3188db341a4
perez6736 <perez6736@gmail.com>
2018-01-06 17:12:23 -0500

    changing env files again

--- controllers/NYTscraper_controller.js
+++ controllers/NYTscraper_controller.js
@@ -13,7 +13,7 @@ var cheerio = require("cheerio");
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 // change this when deploying to heroku v 
-mongoose.connect(process.env.MONGOURL);
+mongoose.connect(process.env.MONGODB_URI);
 
 // Routes ==========================================================
 
