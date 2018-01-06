164ece8d48653d4ea598a5ab9cd35bca20d9ae03
perez6736 <perez6736@gmail.com>
2018-01-06 00:31:18 -0500

    saving articles is now a thing

--- controllers/NYTscraper_controller.js
+++ controllers/NYTscraper_controller.js
@@ -75,14 +75,27 @@ router.get("/scrape", function(req, res){
 
 // send only saved articles to handlebars
 router.get("/save", function(req, res){
-    
+    //so now we need to read from the DB
+    db.Article.find({issaved: true})
+    .then(function(dbArticles) {
+      // If any Books are found, send them to the client
+      console.log(dbArticles);
+      res.render("index", {articles: dbArticles});
+    })
+    .catch(function(err) {
+      // If an error occurs, send it back to the client
+      res.json(err);
+    });
     
 });
 
 // changed the isSaved flag of the article. 
 router.post("/save/:id", function(req, res){
     // get the id of the article and change the isSaved flag to true 
-    
+    console.log(req.body);
+    db.Article.update({_id: req.body.id}, {$set: {issaved: true}}, function(msg){
+        res.send("Saved.")
+    })
     
 });
 
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,21 +2,19 @@ $(document).ready(function(){
     function saveArticle(){
         // Grab the id associated with the article from the submit button
         var thisId = $(this).attr("data-id");
-        console.log("id= " + thisId);
         // Run a POST request to change the note, using what's entered in the inputs
-        // $.ajax({
-        //     method: "POST",
-        //     url: "/save/" + thisId,
-        //     data: {
-        //     // Value taken from title input
-        //     title: $("#titleinput").val(),
-        //     // Value taken from note textarea
-        //     body: $("#bodyinput").val()
-        //     }
-        // })
-        //     // With that done
-        // .done(function(data) {
-        // });
+        $.ajax({
+            method: "POST",
+            url: "/save/" + thisId,
+            data: {
+                id: thisId
+            }
+        })
+            // With that done
+        .done(function(data) {
+            // Log the response
+            console.log(data);
+        });
     }
 
     $(".saveArticle").click(saveArticle);
